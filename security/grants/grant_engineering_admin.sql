20828b21b0625977c4cb09c6f3bb50faf1e3a375
rubenoreamuno <78568009+rubenoreamuno@users.noreply.github.com>
2025-08-16 13:40:35 -0600

    Delete security directory

--- security/grants/grant_engineering_admin.sql
+++
@@ -1,3 +0,0 @@
-GRANT ALL PRIVILEGES ON CATALOG engineering TO `engineering_admin_group`;
-GRANT ALL PRIVILEGES ON SCHEMA engineering.core TO `engineering_admin_group`;
-GRANT ALL PRIVILEGES ON TABLE engineering.core.* TO `engineering_admin_group`;
--- security/grants/grant_finance_analyst.sql
+++
@@ -1,3 +0,0 @@
-GRANT SELECT ON CATALOG finance TO `finance_analyst_group`;
-GRANT SELECT ON SCHEMA finance.reporting TO `finance_analyst_group`;
-GRANT SELECT ON TABLE finance.reporting.transactions TO `finance_analyst_group`;
